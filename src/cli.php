b8627568887366d819ae00ddb395785b78f202fe
rinat salimyanov <rinatsin@gmail.com>
2019-09-15 01:48:10 +0500

    Change composer.jsen

--- src/cli.php
+++ src/cli.php
@@ -1,12 +1,13 @@
 <?php
-namespace BrainGames\Cli;
+
+namespace BrainGames\Cli; 
 
 use function \cli\line;
 use function \cli\prompt;
 
 function run()
 {
-    line("Welcome to the Brain Games!");
+    line("Welcome To The Brain Games!");
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
