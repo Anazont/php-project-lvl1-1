ac0d7d27caaf5ae82fdac34ef9f70dc2ce690527
rinat salimyanov <rinatsin@gmail.com>
2019-09-18 05:05:54 +0500

    Add path_loader

---
+++ src/path_loader.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Command Line functions to interraction with user
+ *
+ * PHP version 7.3
+ *
+ * @category PHP
+ * @package  Php-project-lvl1
+ * @author   Rinat Salimyanov <rinatsin@gmail.com>
+ * @license  https://github.com/Rinatsin/php-project-lvl1 MIT
+ * @link     https://github.com/Rinatsin/php-project-lvl1
+ */
+
+
+namespace BrainGames\Path_loader;
+
+/**
+ * The function is load path   
+ * 
+ * @return void
+ */
+function load()
+{
+    $loadPath1 = __DIR__.'/../../../autoload.php';
+    $loadPath2 = __DIR__.'/../vendor/autoload.php';
+
+    if (file_exists($loadPath1)) {
+        include_once $loadPath1;
+    } else {
+        include_once $loadPath2;
+    }
+}
